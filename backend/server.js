31a79f210f3f3e5b96f76cf7d6ae1cf05013d2bd
arx <arx@123>
2026-01-14 16:08:26 +0530

    server_fix

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,9 @@ if (ENV.NODE_ENV === "production") {
   const frontendPath = path.join(__dirname, "../admin/dist");
 
   console.log("Frontend path:", frontendPath);
+  
   app.use(express.static(frontendPath));
+  
   app.get("/*", (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
